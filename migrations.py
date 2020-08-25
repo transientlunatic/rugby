46e48c109101186f86561ca53a12c5c0607e898e
Daniel Williams <mail@daniel-williams.co.uk>
2020-08-25 19:38:20 +0100

    Added the migrations.

--- migrations.py
+++ migrations.py
@@ -1,2 +1,9 @@
 from rugby.models import *
+if 'RUGBYDB' in os.environ:
+    db = os.environ['RUGBYDB']
+    app.config['JWT_SECRET_KEY'] = os.environ['RUGBY_SECRET']
+else:
+    db = f"{rugby.__path__[0]}/rugby.db"
+    app.config['JWT_SECRET_KEY'] = 'test'
+engine = create_engine(f'sqlite:///{db}')
 ConferenceMap.__table__.create(bind = engine)
